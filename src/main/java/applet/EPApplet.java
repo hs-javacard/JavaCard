eb6843d5c91f757feaad7f55f55dd6fe360bcd9f
Reg Huijben <reg@Regs-MacBook-Pro-1487.local>
2019-06-03 16:16:17 +0200

    AES

--- src/main/java/applet/EPApplet.java
+++ src/main/java/applet/EPApplet.java
@@ -3,6 +3,7 @@ package applet;
 import javacard.framework.*;
 import javacard.security.*;
 import javacardx.crypto.Cipher;
+import javacard.security.RandomData;
 //import javacard.framework.OwnerPIN;
 
 
@@ -14,14 +15,28 @@ public class EPApplet extends Applet implements ISO7816 {
     private OwnerPIN pin;
     private KeyPair keyPair;
 
-    Cipher rsaCipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1,false);
-    private byte[] dummyPinRemoveLater = {0,5};
+    private RandomData rng;
+    private byte[] rndBuffer;
+    RandomData random = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM);
+
+//    rng = random.getInstance(RandomData.ALG_SECURE_RANDOM);
+
+    byte[] theKey = {0x2d, 0x2a, 0x2d, 0x42, 0x55, 0x49, 0x4c, 0x44, 0x41, 0x43, 0x4f, 0x44, 0x45, 0x2d, 0x2a, 0x2d};
+    private AESKey sharedKey;
+
+    Cipher aesCipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
+    Cipher rsaCipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1, false);
+    private byte[] dummyPinRemoveLater = {0, 5};
 
     private byte[] decryptArray;
+    private byte[] aesWorkspace;
 
     private short balance;
 
     private short totalToday;
+    //private byte[] aesKeydata = new byte[32];
+    byte[] ivdata = new byte[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+//    private byte[] hardcodedPlaintext = "Hello!".getBytes();
 
     private short softLimit;
     private short hardLimit;
@@ -38,13 +53,17 @@ public class EPApplet extends Applet implements ISO7816 {
         try {
             keyPair = new KeyPair(KeyPair.ALG_RSA, KeyBuilder.LENGTH_RSA_1024);
             keyPair.genKeyPair();
+            sharedKey = (AESKey) KeyBuilder.buildKey(KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
+
         } catch (CryptoException e) {
             short reason = e.getReason();
             ISOException.throwIt(reason);
         }
 
-        decryptArray = JCSystem.makeTransientByteArray( (short) 128, JCSystem.CLEAR_ON_DESELECT);
-        pin.update(dummyPinRemoveLater,(byte) 0,(byte) 2);
+        rndBuffer = JCSystem.makeTransientByteArray((short) 1, JCSystem.CLEAR_ON_DESELECT);
+        decryptArray = JCSystem.makeTransientByteArray((short) 128, JCSystem.CLEAR_ON_DESELECT);
+        aesWorkspace = JCSystem.makeTransientByteArray((short) 256, JCSystem.CLEAR_ON_DESELECT);
+        pin.update(dummyPinRemoveLater, (byte) 0, (byte) 2);
 
         pin.resetAndUnblock();
         cardNumber = 4;
@@ -79,17 +98,17 @@ public class EPApplet extends Applet implements ISO7816 {
             case 100:
                 switch (ins) {
                     case 0:
-                    RSAPublicKey pb = (RSAPublicKey) keyPair.getPublic();
+                        RSAPublicKey pb = (RSAPublicKey) keyPair.getPublic();
 //                short pbSize = pb.getSize();
-                    short expSize = pb.getExponent(buffer, (short) 4);
+                        short expSize = pb.getExponent(buffer, (short) 4);
 
-                    short modSize = pb.getModulus(buffer, (short) (expSize + 4));
+                        short modSize = pb.getModulus(buffer, (short) (expSize + 4));
 //                Util.arrayCopy(,0,buffer,0,pbSize);
 //                Util.setShort(buffer, (short) 0, (short) pbSize);
-                    Util.setShort(buffer, (short) 0, expSize);
-                    Util.setShort(buffer, (short) 2, modSize);
-                    sendResponse(apdu, (short) (expSize + modSize + 4));
-                    break;
+                        Util.setShort(buffer, (short) 0, expSize);
+                        Util.setShort(buffer, (short) 2, modSize);
+                        sendResponse(apdu, (short) (expSize + modSize + 4));
+                        break;
                     case 1:
                         RSAPrivateKey pk = (RSAPrivateKey) keyPair.getPrivate();
                         rsaCipher.init(pk, Cipher.MODE_DECRYPT);
@@ -99,11 +118,72 @@ public class EPApplet extends Applet implements ISO7816 {
                         break;
                 }
                 break;
+            case 101:
+
+
+                Util.arrayCopy("Henk!".getBytes(), (short) 0, buffer, (short) 0, (short) 5);
+
+
+                short encSize = encrypt(apdu, (short) 5);
+                sendResponse(apdu, encSize);
+                break;
             default:
                 ISOException.throwIt(SW_CLA_NOT_SUPPORTED);
                 break;
         }
     }
+
+    private short encrypt(APDU apdu, short msgSize) {
+        byte[] buffer = apdu.getBuffer();
+
+        Util.arrayCopy(buffer, (short) 0, aesWorkspace, (short) 0, msgSize);
+
+        // figure out the size in blocks
+        short blocks = (short) (msgSize / 16);
+        if ((msgSize % 16) > 0)
+            blocks++;
+
+        short encSize = (short) (blocks * 16);
+        short paddingSize = (short) (encSize - msgSize);
+
+        Util.arrayFillNonAtomic(aesWorkspace, msgSize, paddingSize, (byte) 3);
+
+        // generate IV
+        random.generateData(ivdata, (short) 0, (short) 16);
+
+        sharedKey.setKey(theKey, (short) 0);
+
+        aesCipher.init(sharedKey, Cipher.MODE_ENCRYPT, ivdata, (short) 0, (short) 16);
+        aesCipher.doFinal(aesWorkspace, (short) 0, encSize, buffer, (short) 2);
+
+        short offset = 16 + 2;
+
+        Util.arrayCopy(ivdata, (short) 0, buffer, offset, (short) 16);
+        Util.setShort(buffer, (short) 0, msgSize);
+
+        return (short) (encSize + 2 + 16);
+    }
+
+//    private void decrypt(APDU apdu) {
+//        byte[] buffer = apdu.getBuffer();
+//
+//
+//        short len = Util.getShort(buffer, (short) 0);
+//        short blocks = (short) (len / 16);
+//        if ((len % 16) > 0)
+//            blocks++;
+//
+//        short encSize = (short) (blocks * 16);
+//
+//        Util.arrayCopy(responsedata, (short) (encSize + 2), ivdata, (short) 0, (short) 16);
+//
+//
+//        aesCipher.init(sharedKey, Cipher.MODE_DECRYPT, ivdata, (short) 0, (short) 16);
+//
+//        aesCipher.doFinal(responsedata, (short) 2, (short) encSize, plaintext, (short) 0);
+//        System.out.println(new String(plaintext));
+////
+//    }
 
     private void auth(APDU apdu) {
     }
@@ -164,7 +244,7 @@ public class EPApplet extends Applet implements ISO7816 {
 
         short pin = Util.getShort(buffer, OFFSET_CDATA);
 
-        boolean valid = this.pin.check(buffer,OFFSET_CDATA,(byte) 2); // two bytes long
+        boolean valid = this.pin.check(buffer, OFFSET_CDATA, (byte) 2); // two bytes long
 
         byte statusCode = -1;
         if (valid) {
--- src/test/java/applet/EPAppletTest.java
+++ src/test/java/applet/EPAppletTest.java
@@ -32,92 +32,126 @@ public class EPAppletTest {
         byte p1 = 0;
         byte p2 = 0;
 
-        // check card number
-        byte[] data1 = new byte[]{0, 4};
-        CommandAPDU c1 = new CommandAPDU(cla, 0, p1, p2, data1, 2);
-        ResponseAPDU r1 = sim.transmitCommand(c1);
-
-        short cardNumber = Util.getShort(r1.getData(), (short) 1);
-        assertEquals((short) 4, cardNumber);
-
-        // check soft limit
-        byte[] data2 = new byte[]{0, 0};
-        Util.setShort(data2, (short) 0, (short) 1500);
-        CommandAPDU c2 = new CommandAPDU(cla, 1, p1, p2, data2, 2);
-        ResponseAPDU r2 = sim.transmitCommand(c2);
-
-        System.out.println(r2 + " " + Arrays.toString(r2.getData()));
-
-        byte statusCode2 = r2.getData()[1];
-        assertEquals(-2, statusCode2);
-
-        // check pin
-        byte[] data3 = new byte[]{0, 0};
-        short pin = 4;
-        Util.setShort(data3, (short) 0, (short) pin);
-        CommandAPDU c3 = new CommandAPDU(cla, 2, p1, p2, data3, 2);
-        ResponseAPDU r3 = sim.transmitCommand(c3); // 3x wrong pin
-        ResponseAPDU r4 = sim.transmitCommand(c3);
-//        ResponseAPDU r5 = sim.transmitCommand(c3);
-
-        System.out.println(r3 + " " + Arrays.toString(r3.getData()));
-
-        byte statusCode3 = r3.getData()[1];
-        assertEquals(-1, statusCode3);
-
-
-        byte[] data4 = new byte[]{0, 0};
-        short pin2 = 5;
-        Util.setShort(data4, (short) 0, (short) pin2);
-        CommandAPDU c31 = new CommandAPDU(cla, 2, p1, p2, data4, 2);
-        ResponseAPDU r31 = sim.transmitCommand(c31);
-        byte statusCode31 = r31.getData()[1];
-        assertEquals(1, statusCode31);
-
-
-        byte[] data6 = new byte[]{0,0};
-        CommandAPDU c6 = new CommandAPDU(100, 0, p1, p2, data6, 2);
-        ResponseAPDU r6 = sim.transmitCommand(c6);
-        byte statusC = r6.getData()[1];
-
-        byte[] keyBuffer = r6.getData();
-        short expSize = Util.getShort(keyBuffer,(short) 0);
-        short modSize = Util.getShort(keyBuffer,(short) 2);
-
-        RSAPublicKey cardPub = (RSAPublicKey) KeyBuilder.buildKey(KeyBuilder.TYPE_RSA_PUBLIC, KeyBuilder.LENGTH_RSA_1024, true);
-
-        cardPub.setExponent(keyBuffer,(short) 4, expSize);
-        cardPub.setModulus(keyBuffer,(short) (4 + expSize), modSize);
-
-
-        final byte[] myMessage = "Henk!".getBytes();
-        short nonce = 0;
-        final byte[] msg = new byte[100];
-        Util.setShort(msg,(short) 0,nonce);
-        Util.arrayCopy(myMessage,(short)0 ,msg,(short) 2, (short) myMessage.length);
-
-        System.out.println(cardPub.isInitialized());
-        Cipher rsaCipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1,false);
-//        if (myMessage.length >= modSize.length - 11) {
-//            System.out.println("Data too long!");
-//        }
-        rsaCipher.init(cardPub, Cipher.MODE_ENCRYPT);
-
-
-
-        final byte[] encryptedMsg = JCSystem.makeTransientByteArray(
-                (short) 128, JCSystem.CLEAR_ON_DESELECT);
-//        rsaCipher.doFinal()
-        rsaCipher.doFinal(msg, (short) 0,
-                (short) msg.length, encryptedMsg, (short) 0);
+//        // check card number
+//        byte[] data1 = new byte[]{0, 4};
+//        CommandAPDU c1 = new CommandAPDU(cla, 0, p1, p2, data1, 2);
+//        ResponseAPDU r1 = sim.transmitCommand(c1);
+//
+//        short cardNumber = Util.getShort(r1.getData(), (short) 1);
+//        assertEquals((short) 4, cardNumber);
+//
+//        // check soft limit
+//        byte[] data2 = new byte[]{0, 0};
+//        Util.setShort(data2, (short) 0, (short) 1500);
+//        CommandAPDU c2 = new CommandAPDU(cla, 1, p1, p2, data2, 2);
+//        ResponseAPDU r2 = sim.transmitCommand(c2);
+//
+//        System.out.println(r2 + " " + Arrays.toString(r2.getData()));
+//
+//        byte statusCode2 = r2.getData()[1];
+//        assertEquals(-2, statusCode2);
+//
+//        // check pin
+//        byte[] data3 = new byte[]{0, 0};
+//        short pin = 4;
+//        Util.setShort(data3, (short) 0, (short) pin);
+//        CommandAPDU c3 = new CommandAPDU(cla, 2, p1, p2, data3, 2);
+//        ResponseAPDU r3 = sim.transmitCommand(c3); // 3x wrong pin
+//        ResponseAPDU r4 = sim.transmitCommand(c3);
+////        ResponseAPDU r5 = sim.transmitCommand(c3);
+//
+//        System.out.println(r3 + " " + Arrays.toString(r3.getData()));
+//
+//        byte statusCode3 = r3.getData()[1];
+//        assertEquals(-1, statusCode3);
+//
+//
+//        byte[] data4 = new byte[]{0, 0};
+//        short pin2 = 5;
+//        Util.setShort(data4, (short) 0, (short) pin2);
+//        CommandAPDU c31 = new CommandAPDU(cla, 2, p1, p2, data4, 2);
+//        ResponseAPDU r31 = sim.transmitCommand(c31);
+//        byte statusCode31 = r31.getData()[1];
+//        assertEquals(1, statusCode31);
+//
+//
+//        byte[] data6 = new byte[]{0,0};
+//        CommandAPDU c6 = new CommandAPDU(100, 0, p1, p2, data6, 2);
+//        ResponseAPDU r6 = sim.transmitCommand(c6);
+//        byte statusC = r6.getData()[1];
+//
+//        byte[] keyBuffer = r6.getData();
+//        short expSize = Util.getShort(keyBuffer,(short) 0);
+//        short modSize = Util.getShort(keyBuffer,(short) 2);
+//
+//        RSAPublicKey cardPub = (RSAPublicKey) KeyBuilder.buildKey(KeyBuilder.TYPE_RSA_PUBLIC, KeyBuilder.LENGTH_RSA_1024, true);
+//
+//        cardPub.setExponent(keyBuffer,(short) 4, expSize);
+//        cardPub.setModulus(keyBuffer,(short) (4 + expSize), modSize);
+//
+////
+//        final byte[] myMessage = "Henk!".getBytes();
+//        short nonce = 0;
+//        final byte[] msg = new byte[118];
+//        Util.setShort(msg,(short) 0,nonce);
+//        Util.arrayCopy(myMessage,(short)0 ,msg,(short) 2, (short) myMessage.length);
+//
+//        System.out.println(cardPub.isInitialized());
+//        Cipher rsaCipher = Cipher.getInstance(Cipher.ALG_RSA_PKCS1,false);
+////        if (myMessage.length >= modSize.length - 11) {
+////            System.out.println("Data too long!");
+////        }
+//        rsaCipher.init(cardPub, Cipher.MODE_ENCRYPT);
+//
+//
+//
+//        final byte[] encryptedMsg = JCSystem.makeTransientByteArray(
+//                (short) 128, JCSystem.CLEAR_ON_DESELECT);
+////        rsaCipher.doFinal()
+//        rsaCipher.doFinal(msg, (short) 0,
+//                (short) msg.length, encryptedMsg, (short) 0);
 
 //        System.out.println();
 
+//
+//        byte[] data7 = new byte[128];
+//        Util.arrayCopy(encryptedMsg,(short) 0, data7,(short) 0,(short) 128);
+//        CommandAPDU c7 = new CommandAPDU(100, 1, p1, p2, data7, 2);
+//        ResponseAPDU r7 = sim.transmitCommand(c7);
 
-        byte[] data7 = new byte[128];
-        Util.arrayCopy(encryptedMsg,(short) 0, data7,(short) 0,(short) 128);
-        CommandAPDU c7 = new CommandAPDU(100, 1, p1, p2, data7, 2);
-        ResponseAPDU r7 = sim.transmitCommand(c7);
+        byte[] theKey = {0x2d, 0x2a, 0x2d, 0x42, 0x55, 0x49, 0x4c, 0x44, 0x41, 0x43, 0x4f, 0x44, 0x45, 0x2d, 0x2a, 0x2d};
+        RandomData random = RandomData.getInstance(RandomData.ALG_SECURE_RANDOM);
+        Cipher aesCipher = Cipher.getInstance(Cipher.ALG_AES_BLOCK_128_CBC_NOPAD, false);
+        byte[] ivdata = new byte[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        byte[] plaintext = new byte[32];
+        AESKey sharedKey = (AESKey) KeyBuilder.buildKey (KeyBuilder.TYPE_AES, KeyBuilder.LENGTH_AES_128, false);
+
+        byte[] data8 = new byte[]{0,0};
+        CommandAPDU c8 = new CommandAPDU(101, 0, p1, p2, data8, 2);
+        ResponseAPDU r8 = sim.transmitCommand(c8);
+        byte statusC8 = r8.getData()[1];
+        byte[] responsedata = r8.getData();
+
+        sharedKey.setKey(theKey, (short) 0);
+
+        short len = Util.getShort(responsedata, (short) 0);
+        short blocks = (short) (len/16);
+        if ((len % 16) > 0 ) {
+            blocks++;
+        }
+        short encSize = (short) (blocks*16);
+
+
+
+
+        Util.arrayCopy(responsedata, (short) (encSize + 2), ivdata,(short) 0, (short) 16);
+
+
+
+        aesCipher.init(sharedKey, Cipher.MODE_DECRYPT, ivdata, (short) 0 , (short) 16);
+
+        aesCipher.doFinal(responsedata, (short) 2, (short) encSize, plaintext, (short) 0);
+        System.out.println( new String(plaintext));
 //        byte statusC = r7.getData()[1];
 
 //        if (!isRSAKeyInitialized) {
